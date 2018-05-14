2ccde318e41b9805d4bbdbcccf6835eae0ac981a
Alex Gula <alexgul30@gmail.com>
2018-05-14 14:51:37 +0300

    posts and users update

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,16 @@
+
+
 let PostsHandler = require('../handlers/posts');
 let express = require('express');
 let router = express.Router();
 let postsHandler = new PostsHandler();
 
 router.get('/', postsHandler.getAllPosts);
+router.get('/getWithUsers', postsHandler.getPostsWithUser);
+router.get('/getByData',postsHandler.getPostsByUserByDate);
 router.get('/:id', postsHandler.getPostById);
 router.get('/authors/:userId', postsHandler.getPostsByUserId);
-router.get('/getWithUsers', postsHandler.getPostsWithUser);
+
 router.post('/',postsHandler.createPost);
 router.patch('/:id',postsHandler.updatePost);
 router.delete('/:id',postsHandler.deletePost);
