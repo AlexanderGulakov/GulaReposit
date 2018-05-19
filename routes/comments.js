093cf5d5f64deea561d681122ccf91d76954c400
Alex Gula <alexgul30@gmail.com>
2018-05-20 01:17:45 +0300

    add updating and deleting comments, lookup posts with comments with authors of comments

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,11 @@ let router = express.Router();
 let commentsHandler = new CommentsHandler();
 let checkAuthentication = require('../helpers/session').checkAuthentication;
 
-
+router.get('/:id',commentsHandler.getById);
+router.get('/', commentsHandler.getAllComments);
 router.post('/addComment', checkAuthentication, commentsHandler.addComment);
+router.patch('/:id',checkAuthentication, commentsHandler.updateComment);
+router.delete('/:id',checkAuthentication, commentsHandler.deleteComment);
+
 
 module.exports = router;
